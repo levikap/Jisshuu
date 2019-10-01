c17f0832f33aacb7347db24669e0e89df72ca33f
lkaplan <kaplan.l@husky.neu.edu>
2019-09-30 22:22:26 -0400

    added fetching website

--- Jisshuu/ViewController.swift
+++ Jisshuu/ViewController.swift
@@ -6,13 +6,25 @@
 //  Copyright © 2019 Levi Kaplan. All rights reserved.
 //
 
-import UIKit
+import UIKit 
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let url = URL(string: "https://levikap.github.io/portfolio/2019/09/28/second-post.html") {
+            do {
+                let contents = try String(contentsOf: url)
+                print(contents)
+            } catch {
+                // contents could not be loaded
+            }
+        } else {
+            // the URL was bad!
+        }
+        
     }
 
 
