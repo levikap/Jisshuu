11429a56599454603929d769ad8bac4ab2c7fdd2
lkaplan <kaplan.l@husky.neu.edu>
2019-10-18 17:01:24 -0400

    swiftUI changes'

---
+++ Jisshuu/Jisshuu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jisshuu
+//
+//  Created by Levi Kaplan on 10/17/19.
+//  Copyright © 2019 Levi Kaplan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jisshuu/Jisshuu/Divider.swift
@@ -0,0 +1,21 @@
+//
+//  Divider.swift
+//  Jisshuu
+//
+//  Created by Levi Kaplan on 10/18/19.
+//  Copyright © 2019 Levi Kaplan. All rights reserved.
+//
+
+import SwiftUI
+
+struct Divider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Divider_Previews: PreviewProvider {
+    static var previews: some View {
+        Divider()
+    }
+}
